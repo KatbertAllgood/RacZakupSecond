7c64dcb68b3ad4adc434226585b97b7c2543f205
KatbertAllgood <srazmi@mail.ru>
2023-02-17 20:35:51 +0300

    added splash screen

--- app/src/main/java/com/example/raczakupsecond/screens/MainActivity.kt
+++ app/src/main/java/com/example/raczakupsecond/screens/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.raczakupsecond.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.raczakupsecond.R
 import com.example.raczakupsecond.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_RacZakupSecond)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
