757c95d1114c5722179300101684251daaa08902
KatbertAllgood <srazmi@mail.ru>
2023-02-25 09:43:17 +0300

    interceptors

---
+++ data/src/main/java/com/example/data/network/api/interceptors/CookieInterceptor.kt
@@ -0,0 +1,30 @@
+package com.example.data.network.api.interceptors
+
+import android.util.Log
+import com.example.data.utils.InterceptorsPreferences
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class CookieInterceptor(
+//    context: Context
+) : Interceptor {
+//    private val repository = SharedPreferencesRepositoryImpl(context)
+//    private val updatePreferenceUseCase = UpdatePreferenceUseCase(repository)
+
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder()
+            .build()
+        val response = chain.proceed(request)
+        if (response.headers("set-cookie").isNotEmpty()) {
+//            updatePreferenceUseCase.invoke(Constants.SET_COOKIE, response.headers("set-cookie"))
+            InterceptorsPreferences.setCookie = response.headers("set-cookie").toString()
+                .replace("[", "")
+                .replace("]", "")
+
+            val _cookie: String = InterceptorsPreferences.setCookie ?: ""
+            Log.d("COOKIE", _cookie)
+        }
+        return response
+    }
+}
---
+++ data/src/main/java/com/example/data/utils/InterceptorsPreferences.kt
@@ -0,0 +1,53 @@
+package com.example.data.utils
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import androidx.core.content.edit
+
+//GET VALUES
+//val heightInCentimeters: Int? = AppPreferences.heightInCentimeters
+//val heightOrDefault: Int = AppPreferences.heightInCentimeters ?: 170
+//
+//SET VALUES
+//AppPreferences.heightInCentimeters = 160
+
+object InterceptorsPreferences {
+
+    private var sharedPreferences: SharedPreferences? = null
+
+    fun setup(context: Context) {
+        sharedPreferences = context.getSharedPreferences("cookie_table", MODE_PRIVATE)
+    }
+
+    var setAccess: String?
+        get() = Key.ACCESS_TOKEN.getString()
+        set(value) = Key.ACCESS_TOKEN.setString(value)
+
+    var setRefresh: String?
+        get() = Key.REFRESH_TOKEN.getString()
+        set(value) = Key.REFRESH_TOKEN.setString(value)
+
+    var setCookie: String?
+        get() = Key.SET_COOKIE.getString()
+        set(value) = Key.SET_COOKIE.setString(value)
+
+    private enum class Key {
+        ACCESS_TOKEN, REFRESH_TOKEN, SET_COOKIE;
+
+        fun getBoolean(): Boolean? = if (sharedPreferences!!.contains(name)) sharedPreferences!!.getBoolean(name, false) else null
+        fun getFloat(): Float? = if (sharedPreferences!!.contains(name)) sharedPreferences!!.getFloat(name, 0f) else null
+        fun getInt(): Int? = if (sharedPreferences!!.contains(name)) sharedPreferences!!.getInt(name, 0) else null
+        fun getLong(): Long? = if (sharedPreferences!!.contains(name)) sharedPreferences!!.getLong(name, 0) else null
+        fun getString(): String? = if (sharedPreferences!!.contains(name)) sharedPreferences!!.getString(name, "") else null
+
+        fun setBoolean(value: Boolean?) = value?.let { sharedPreferences!!.edit { putBoolean(name, value) } } ?: remove()
+        fun setFloat(value: Float?) = value?.let { sharedPreferences!!.edit { putFloat(name, value) } } ?: remove()
+        fun setInt(value: Int?) = value?.let { sharedPreferences!!.edit { putInt(name, value) } } ?: remove()
+        fun setLong(value: Long?) = value?.let { sharedPreferences!!.edit { putLong(name, value) } } ?: remove()
+        fun setString(value: String?) = value?.let { sharedPreferences!!.edit { putString(name, value) } } ?: remove()
+
+        fun exists(): Boolean = sharedPreferences!!.contains(name)
+        fun remove() = sharedPreferences!!.edit { remove(name) }
+    }
+}
