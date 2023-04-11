3a113ed354712645e29b06671e558a4933129135
KatbertAllgood <srazmi@mail.ru>
2023-04-11 21:12:37 +0300

    test

--- app/src/main/java/com/example/raczakupsecond/screens/profile/profilepage/adapters/ProfileFamilyGroupAdapter.kt
+++ app/src/main/java/com/example/raczakupsecond/screens/profile/profilepage/adapters/ProfileFamilyGroupAdapter.kt
@@ -14,6 +14,9 @@ import java.util.*
 
 
 
+
+
+
 class ProfileFamilyGroupAdapter(
     private val familiesList: List<NewFamilyDomain>
 ) : RecyclerView.Adapter<ProfileFamilyGroupAdapter.FamilyGroupHolder>() {
