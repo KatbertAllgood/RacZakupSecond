b197d3863a2d3b6532a74985e7b8f6a300a5a3ad
KatbertAllgood <srazmi@mail.ru>
2023-04-20 02:26:44 +0300

    modifying

--- app/src/main/java/com/example/raczakupsecond/screens/packs/checkpack/CheckPackFragment.kt
+++ app/src/main/java/com/example/raczakupsecond/screens/packs/checkpack/CheckPackFragment.kt
@@ -1,16 +1,13 @@
 package com.example.raczakupsecond.screens.packs.checkpack
 
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.domain.models.packs.HealthySetParamsDomain
 import com.example.raczakupsecond.R
 import com.example.raczakupsecond.databinding.FragmentCheckPackBinding
 import com.example.raczakupsecond.screens.packs.checkpack.adapters.CheckPackFamilyMembersAdapter
